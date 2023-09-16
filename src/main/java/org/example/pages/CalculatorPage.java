cd3c147e951e757864eca75cdc422a66487d7946
Le Marcell <73469368+felhos@users.noreply.github.com>
2023-09-16 16:25:04 +0200

    create POM

---
+++ src/main/java/org/example/pages/CalculatorPage.java
@@ -0,0 +1,11 @@
+package org.example.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class CalculatorPage {
+    private WebDriver driver;
+
+    public CalculatorPage(WebDriver driver) {
+        this.driver = driver;
+    }
+}
