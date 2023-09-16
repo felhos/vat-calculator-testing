6fb0eaecdde2cf64bfd0decf91b777d9faa46972
Le Marcell <73469368+felhos@users.noreply.github.com>
2023-09-17 01:23:58 +0200

    add reference calculator

---
+++ src/main/java/org/example/util/ReferenceCalculator.java
@@ -0,0 +1,13 @@
+package org.example.util;
+
+public class ReferenceCalculator {
+    public static ResultSet calculateFromNet(float net) {
+        return null;
+    }
+    public static ResultSet calculateFromVat(float vat) {
+        return null;
+    }
+    public static ResultSet calculateFromGross(float gross) {
+        return null;
+    }
+}
---
+++ src/main/java/org/example/util/ResultSet.java
@@ -0,0 +1,4 @@
+package org.example.util;
+
+public record ResultSet(float netPrice, float vatValue, float grossPrice) {
+}
