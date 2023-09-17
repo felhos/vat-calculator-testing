79be9311a12409023025781305015de571127166
Le Marcell <73469368+felhos@users.noreply.github.com>
2023-09-17 23:27:09 +0200

    add beforeEach method

--- src/test/java/org/example/pages/CalculatorPageBaseTest.java
+++ src/test/java/org/example/pages/CalculatorPageBaseTest.java
@@ -1,7 +1,24 @@
 package org.example.pages;
 
-import static org.junit.jupiter.api.Assertions.*;
 
-class CalculatorPageTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+abstract class CalculatorPageBaseTest {
+    private String projectRoot = System.getProperty("user.dir");
+    private String driverPath = projectRoot + "/src/main/resources/drivers/chromedriver.exe";
+    private WebDriver driver;
+    private CalculatorPage calculatorPage;
+
+    @BeforeEach
+    public void setUp() {
+        // Set up WebDriver
+        System.setProperty("webdriver.chrome.driver", driverPath);
+        driver = new ChromeDriver();
+        driver.get("https://www.calkoo.com/en/vat-calculator");
+        calculatorPage = new CalculatorPage(driver);
+        calculatorPage.acceptCookies();
+    }
 
 }
